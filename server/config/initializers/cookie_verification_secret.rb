47f6d5812423ac712637664155e9b930552005e7
Caitlin <caitlin@ubuntu.(none)>
2013-07-16 14:17:25 -0700

    links on home page and time/timezone ddisplay

---
+++ server/config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '690ffe0d2ba5aa775ac09b33a089fa83549ebe002c5fab834ca2268c48db1ed5eff3605ed4ecc2806fcc817c54ece02c0718f300ce884df8c2603294ae5d9270';
